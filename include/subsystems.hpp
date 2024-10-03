259e741c1a1e2d9fe47fc2cfbab548b3d0ec13c7
ECR-404 <ecr.robotics@yahoo.com>
2024-10-03 15:32:34 -0700

    a

---
+++ include/subsystems.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "api.h"
+
+// Your motors, sensors, etc. should go here.  Below are examples
+
+inline pros::Motor Motor1 (7);
+inline pros::Motor Motor2 (14);
+inline pros::Optical optical (20);
+
+inline ez::Piston Piston1('B');
+// inline pros::Motor intake(1);
+// inline pros::adi::DigitalIn limit_switch('A');
